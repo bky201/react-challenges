37d204c2a6f3910849169889e0830834ede2298b
bek <bekrye@gmail.com>
2023-08-31 14:10:44 +0200

    Creating a map function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
+// import Sidebar from './components/Sidebar';
 // import NavBarSimple from './components/NavBarSimple';
-import NavBarForm from './components/NavBarForm';
+// import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
+
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
+      {/* <Sidebar /> */}
       {/* <NavBarSimple /> */}
-      <NavBarForm />
+      {/* <NavBarForm /> */}
+      <Content />
+
 
     </div>
   );
