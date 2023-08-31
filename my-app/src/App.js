48ad58989944ae86d1f4dcc01023b978d3275b8e
bek <bekrye@gmail.com>
2023-09-01 01:19:40 +0200

    Add loader challeng and search feature challeng

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
 import css from './App.module.css';
-// import Sidebar from './components/Sidebar';
+import Sidebar from './components/Sidebar';
 // import NavBarSimple from './components/NavBarSimple';
-// import NavBarForm from './components/NavBarForm';
+import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* <Sidebar /> */}
+      <NavBarForm />
+      <Sidebar />
       {/* <NavBarSimple /> */}
-      {/* <NavBarForm /> */}
       <Content />
 
 
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
 import css from "./css/Content.module.css";
-import {savedPosts} from "../posts.json";
+import { savedPosts } from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from './Loader';
 
 export class Content extends Component {
 
-    constructor(props) {
-      super(props)
-    
-      
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoaded: false,
+         posts: [],
     }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true,
+        posts: savedPosts
+      })
+    }, 2000)
+  }
+
+  handleChange = (e) => {
+    const name = e.target.value.toLowerCase();
+    const filteredPosts = savedPosts.filter((post) => {
+      return post.name.toLowerCase().includes(name);
+    })
+
+    this.setState({
+      posts: filteredPosts
+    })
+  }
+
+
   render() {
     return (
       <div className={css.Content}>
+
         <div className={css.TitleBar}>
-            <h1>My Photos</h1>
+          <h1>My Photos</h1>
+          <form>
+            <label htmlFor='searchinput'>Search</label>
+            <input
+              type='search'
+              id='searchinput'
+              placeholder='By Author'
+              onChange={(e) => this.handleChange(e)}
+            />
+            <h4>posts found {this.state.posts.length}</h4>
+          </form>
         </div>
         <div className={css.SearchResults}>
-            {/* part-1 Creating the map function */}
-            {/* {
+          {/* part-1 Creating the map function */}
+          {/* {
             savedPosts.map((post) => {
                 return (
                 <div className={css.SearchItem} key={post.title}>
@@ -31,8 +67,12 @@ export class Content extends Component {
             })
             } */}
 
-            {/* Part 2: Creating a child component */}
-            <PostItem savedPosts={savedPosts} />
+          {/* Part 2: Creating a child component */}
+          {
+            this.state.isLoaded ?
+              <PostItem savedPosts={this.state.posts} />
+              : <Loader />
+          }
         </div>
       </div>
     )
